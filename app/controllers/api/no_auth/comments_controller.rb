ffeabbad64a7b73b2b66d3084c68e205e04ff2f6
Kevin Glowacz <kevin@glowacz.info>
2015-04-20 14:38:04 -0500

    No auth specific controller

--- app/controllers/api/no_auth/comments_controller.rb
+++ app/controllers/api/no_auth/comments_controller.rb
@@ -1,24 +1,28 @@
-class CommentsController < ApplicationController
+class Api::NoAuth::CommentsController < Api::ApiController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
   # GET /comments
   # GET /comments.json
   def index
     @comments = Comment.all
+    render "comments/index"
   end
 
   # GET /comments/1
   # GET /comments/1.json
   def show
+    render "comments/show"
   end
 
   # GET /comments/new
   def new
     @comment = Comment.new
+    render "comments/new"
   end
 
   # GET /comments/1/edit
   def edit
+    render "comments/edit"
   end
 
   # POST /comments
@@ -28,8 +32,8 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
+        format.html { redirect_to path_prefix << @comment, notice: 'Comment was successfully created.' }
+        format.json { render "comments/show", status: :created, location: path_prefix << @comment }
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -42,10 +46,10 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
+        format.html { redirect_to path_prefix << @comment, notice: 'Comment was successfully updated.' }
+        format.json { render "comments/show", status: :ok, location: path_prefix << @comment  }
       else
-        format.html { render :edit }
+        format.html { render "comments/edit" }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
@@ -70,5 +74,9 @@ class CommentsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
       params.require(:comment).permit(:author, :body)
+    end
+
+    def path_prefix
+      ["api", "no_auth"]
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :comments
+  namespace :api do
+    namespace :no_auth do
+      resources :comments
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
