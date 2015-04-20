8a75c653095255b571b7db0f0a31705c1e8a3b67
Kevin Glowacz <kevin@glowacz.info>
2015-04-20 14:05:52 -0500

    null false all the things

--- db/migrate/20150420190424_create_comments.rb
+++ db/migrate/20150420190424_create_comments.rb
@@ -1,10 +1,10 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.string :author
-      t.text :body
+      t.string  :author,  null: false
+      t.text    :body,    null: false
 
-      t.timestamps null: false
+      t.timestamps        null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420184144) do
+ActiveRecord::Schema.define(version: 20150420190424) do
 
   create_table "comments", force: :cascade do |t|
-    t.string   "author"
-    t.text     "comment"
+    t.string   "author",     null: false
+    t.text     "body",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
